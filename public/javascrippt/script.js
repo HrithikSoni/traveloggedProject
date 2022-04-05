9cc2905e041b9974ad56c4b5d7ac99ac40534d50
Hrithik Soni <88163170+HrithikSoni@users.noreply.github.com>
2022-04-05 23:17:06 +0530

    Add files via upload

---
+++ public/javascrippt/script.js
@@ -0,0 +1,8 @@
+// Navbar
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
